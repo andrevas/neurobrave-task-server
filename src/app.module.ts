09a13841ecdbfcb88f3abd5e7bf380c6207bf36f
Andrey Vaschenko <andreva.w@gmail.com>
2023-08-05 15:50:53 +0300

    add dotenv

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,8 +3,10 @@
 import { Module } from '@nestjs/common';
 import { StreamsGateway } from './streams/streams.gateway';
 import { StreamsService } from './streams/streams.service';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
+  imports: [ConfigModule.forRoot()],
   providers: [StreamsGateway, StreamsService],
 })
 export class AppModule {}
