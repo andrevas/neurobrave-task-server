5935b96a99b2633996b1f3855489f5cd2a0ddcec
Andrey Vaschenko <andreva.w@gmail.com>
2023-08-05 15:06:21 +0300

    heroku port fix

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,6 @@ import { IoAdapter } from '@nestjs/platform-socket.io';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useWebSocketAdapter(new IoAdapter(app));
-  await app.listen(3001);
+  await app.listen(process.env.PORT || 3001);
 }
 bootstrap();
